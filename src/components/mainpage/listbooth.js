0d3b722711385c62112a24e6e9b7c3a864a5e488
dxnguyen <dung.xuan.nguyen@mgm-tp.com>
2021-06-28 12:13:12 +0700

    fixed build metlify

--- src/components/mainpage/listbooth.js
+++ src/components/mainpage/listbooth.js
@@ -7,9 +7,9 @@ class ListBooth extends React.Component {
 		return <div className="tm-paging-links">
 			<nav>
 				<ul>
-					<li className="tm-paging-item"><a href="#" className="tm-paging-link active">Xiên Nướng</a></li>
-					<li className="tm-paging-item"><a href="#" className="tm-paging-link">Gỏi</a></li>
-					<li className="tm-paging-item"><a href="#" className="tm-paging-link">Phá Lấu</a></li>
+					<li className="tm-paging-item"><a href="" className="tm-paging-link active">Xiên Nướng</a></li>
+					<li className="tm-paging-item"><a href="" className="tm-paging-link">Gỏi</a></li>
+					<li className="tm-paging-item"><a href="" className="tm-paging-link">Phá Lấu</a></li>
 				</ul>
 			</nav>
 		</div>;
